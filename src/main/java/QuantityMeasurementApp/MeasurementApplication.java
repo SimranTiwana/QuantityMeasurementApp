59f42bf4ffaac0621805e6dbd582708757fbf84e
SimranTiwana <Tiwanasimran010@gmail.com>
2026-03-26 13:52:05 +0530

    UC1: Feet equality implemented

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -3,7 +3,7 @@ package QuantityMeasurementApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class MeasurementApplication {
 	static class Feet{
 		private final double value;
